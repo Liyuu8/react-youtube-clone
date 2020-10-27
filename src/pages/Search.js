9787902750debde6ec4a0a6d6caf138ef42007bc
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-28 01:12:52 +0900

    Update the Header UI

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Layout from '../components/layout/Layout';
 
 const Search = () => {
-  return <div>Search Page</div>;
+  return <Layout>Search Page</Layout>;
 };
 
 export default Search;
--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Layout from '../components/layout/Layout';
 
 const Watch = () => {
-  return <div>Watch Page</div>;
+  return <Layout>Watch Page</Layout>;
 };
 
 export default Watch;
