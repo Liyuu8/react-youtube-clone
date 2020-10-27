35515eed0bf630a35a42880d7cede764009fe9fa
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-28 02:12:30 +0900

    Implement global state

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Layout from '../components/layout/Layout';
 import { fetchPopularData } from '../apis/index';
+import { Store } from '../store';
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
   useEffect(() => {
-    fetchPopularData().then((response) => console.log('data', response));
+    fetchPopularData().then((response) => {
+      console.log('data', response);
+      setGlobalState({
+        type: 'SET_POPULAR',
+        payload: { popular: response.data.items },
+      });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <Layout>Top Page</Layout>;
 };
