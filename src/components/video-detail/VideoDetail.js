94f2a5b0cea4d0e0c73cc1273cb2aadd46d4cf5e
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-29 00:51:09 +0900

    Implement the video player

--- src/components/video-detail/VideoDetail.js
+++ src/components/video-detail/VideoDetail.js
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { fetchSelectedData } from '../../apis';
 import { Store } from '../../store/index';
+import VideoPlay from '../video-play/VideoPlay';
+import Style from './VideoDetail.module.sass';
+import Linkify from 'react-linkify';
 
 const VideoDetail = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -18,7 +21,18 @@ const VideoDetail = () => {
     setSelectedVideo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div></div>;
+  return globalState.selected && globalState.selected.id ? (
+    <div className={Style.wrap}>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Linkify>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Linkify>
+    </div>
+  ) : (
+    <span>no data</span>
+  );
 };
 
 export default VideoDetail;
