866df9418fdd32f157e6d40c51c92227588eb0c3
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-31 21:50:36 +0900

    Add comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './pages/Search';
 import Watch from './pages/Watch';
 
 const App = () => (
+  // https://qiita.com/ry-itto/items/d11ae12dbe9cd32d83f8
   <Router basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={Top}></Route>
